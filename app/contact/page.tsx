9e168f83ab760e2adb92275b1cfbb20f40a5ab76
Alfredo R. Pabon <alfredrafael@gmail.com>
2026-01-15 10:33:11 -0600

    Final touch

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,7 +9,7 @@ export default function Contact() {
     <>
       <PageHeader
         title="Get in Touch"
-        subtitle="Lorem Ipsum Dolor Sit Amet Consectetur"
+        subtitle="For inquiries, project discussions, or collaborations, feel free to reach out via our phone, email, or  contact form!"
         imgSrc="https://dacarch.com/wp-content/uploads/2026/01/arch-image-scaled.jpg"
       />
 
