640fa9a4bef2b91ab9bc535c7bcfff879853dcbd
Alfredo Pabon <pabonfamily@Pabons-Mac-mini.local>
2026-08-28 21:21:06 -0500

    Update Dean Picture

--- components/layout/about.tsx
+++ components/layout/about.tsx
@@ -11,7 +11,7 @@ export default function About() {
       <Prose>
         <section className="about-section">
           <Image
-            src="https://dacarch.com/wp-content/uploads/2026/08/DAC-Main-Architect-1.png"
+            src="https://dacarch.com/wp-content/uploads/2026/08/Dean-Main-Architect-scaled.png"
             alt="Dean Corsaro"
             width={200}
             height={200}
